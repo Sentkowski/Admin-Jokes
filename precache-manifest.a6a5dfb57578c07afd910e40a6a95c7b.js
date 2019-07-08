02ce1389bafba9d8217906a62dba9d0e8a26c2f4
Szymon <39162021+Sentkowski@users.noreply.github.com>
2019-07-08 23:17:21 +0200

    Updates

---
+++ precache-manifest.a6a5dfb57578c07afd910e40a6a95c7b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9d929229b5485fcfcadfae45fef5d4",
+    "url": "/Admin-Jokes/index.html"
+  },
+  {
+    "revision": "e71198850ac95cbb94bf",
+    "url": "/Admin-Jokes/static/css/main.6f1e42fe.chunk.css"
+  },
+  {
+    "revision": "cc9aa9c4577d2bc3a982",
+    "url": "/Admin-Jokes/static/js/2.83505c5b.chunk.js"
+  },
+  {
+    "revision": "e71198850ac95cbb94bf",
+    "url": "/Admin-Jokes/static/js/main.9cb4da49.chunk.js"
+  },
+  {
+    "revision": "5ac23639b554f820e75e",
+    "url": "/Admin-Jokes/static/js/runtime~main.684db663.js"
+  },
+  {
+    "revision": "9f9cbc039af973ac5147b6c00b6d4299",
+    "url": "/Admin-Jokes/static/media/avatar_boy.9f9cbc03.jpg"
+  },
+  {
+    "revision": "e79ac75a9a7a9dfc7aac6e9bcbb6ad7a",
+    "url": "/Admin-Jokes/static/media/avatar_dog.e79ac75a.jpg"
+  },
+  {
+    "revision": "b80ddd8abc373bcf858475df988f9eb2",
+    "url": "/Admin-Jokes/static/media/avatar_lenny.b80ddd8a.png"
+  },
+  {
+    "revision": "e3fbe5434919819d6448d7c2788fcd64",
+    "url": "/Admin-Jokes/static/media/avatar_medieval.e3fbe543.jpg"
+  },
+  {
+    "revision": "e951ce69d3ddd8666fa2947222659a7e",
+    "url": "/Admin-Jokes/static/media/bg.e951ce69.svg"
+  },
+  {
+    "revision": "13793f3e04bc6d77084b215fda64c358",
+    "url": "/Admin-Jokes/static/media/counter-corner-secondary.13793f3e.svg"
+  },
+  {
+    "revision": "56abafb171edb5aeba5d9bbe5b088a7e",
+    "url": "/Admin-Jokes/static/media/heart-black.56abafb1.svg"
+  },
+  {
+    "revision": "0bd236100824ed622ca948a829228211",
+    "url": "/Admin-Jokes/static/media/quot_marks.0bd23610.svg"
+  }
+]);
